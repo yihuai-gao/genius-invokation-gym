361ecb10e59c19012290c731f2451fa88531e632
Lengyue <lengyue@lengyue.me>
2022-12-12 00:27:11 -0500

    update lint tool

--- tools/lint.py
+++ tools/lint.py
@@ -1,5 +1,5 @@
 import subprocess as sp
 
 # Lint using black and isort
-sp.run(["black", "gisim"])
-sp.run(["isort", "gisim", "--profile", "black"])
+sp.run(["black", "gisim", "tools"])
+sp.run(["isort", "gisim", "tools", "--profile", "black"])
--- tools/test.py
+++ tools/test.py
@@ -1,8 +1,8 @@
 import subprocess as sp
 
 # Test using black and isort
-sp.run(["black", "gisim", "--check"])
-sp.run(["isort", "gisim", "--profile", "black", "--check-only"])
+sp.run(["black", "gisim", "tools", "--check"])
+sp.run(["isort", "gisim", "tools", "--profile", "black", "--check-only"])
 
 # Test using pytest
 # sp.run(["pytest", "gisim"])
