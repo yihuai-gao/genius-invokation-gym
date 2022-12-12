4a56f922175967dc9203364468b679200bdeb21a
Lengyue <lengyue@lengyue.me>
2022-12-12 05:24:56 -0500

    add override demo

--- gisim/cards/characters/Cryo/KamisatoAyaka.py
+++ gisim/cards/characters/Cryo/KamisatoAyaka.py
@@ -1,1 +1,18 @@
 """神里绫华"""
+
+from ..base import CharacterSkill, register_character_skill_factory
+
+
+@register_character_skill_factory(12022)
+class KamisatoAyakaSenho(CharacterSkill):
+    """
+    Kamisato Art: Senho / 神里流·霰步
+    (Passive) When switched to be the active character, this character gains <color=#FFFFFFFF>Cryo Elemental Infusion</color>.
+    【被动】此角色被切换为「出战角色」时，附属<color=#FFFFFFFF>冰元素附魔</color>。
+    """
+
+    def on_skill(self) -> list:
+        # This is just a placeholder, you need to replace it with your own implementation
+        return self._build_message(
+            "Inufsion", ("cryo",), conditions={"on_switch": True}
+        )
