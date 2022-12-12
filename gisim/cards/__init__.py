bc2fe0ddba8422db1b859c162971bdc64f22a94a
Lengyue <lengyue@lengyue.me>
2022-12-12 03:14:47 -0500

    optimize import

--- gisim/cards/__init__.py
+++ gisim/cards/__init__.py
@@ -1,5 +1,1 @@
-from .characters import *
-from .equipments import *
-from .events import *
-from .get_card import *
-from .supports import *
+from .characters import CHARACTER_CARDS, CHARACTER_SKILLS
