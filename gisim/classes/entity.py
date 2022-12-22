fb86c1a669dbcba4a3faf3ba3661f9b708849ac2
David Gao <davidgao1013@gmail.com>
2022-12-22 22:22:54 +0800

    Update entities

--- gisim/classes/entity.py
+++ gisim/classes/entity.py
@@ -1,11 +1,69 @@
 """The most basic class for all elements in the game
 """
 import logging
+from queue import PriorityQueue
 import uuid
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from gisim.classes.message import Message
 
 
 class Entity(ABC):
     def __init__(self):
         self._uuid = uuid.uuid4()
         self._logger = logging.getLogger("gisim")
+        
+    @abstractmethod
+    def encode(self) -> dict:
+        ...
+        
+    @abstractmethod
+    def msg_handler(self, msg_queue:PriorityQueue[Message]) -> bool:
+        """Return value should be a boolean to indicate whether the message queue is updated"""
+        ...
+        
+class TalentEntity(Entity):
+    def __init__(self, name:str):
+        self.name = name
+    
+    def encode(self):
+        return {"name": self.name}
+
+    def msg_handler(self, msg_queue: PriorityQueue[Message]) -> bool:
+        updated = False
+        return updated
+
+
+class WeaponEntity(Entity):
+    def __init__(self, name:str):
+        self.name = name
+        
+    def encode(self):
+        return {"name": self.name}
+
+    def msg_handler(self, msg_queue: PriorityQueue[Message]) -> bool:
+        updated = False
+        return updated
+    
+class ArtifactEntity(Entity):
+    def __init__(self, name:str):
+        self.name = name
+        
+    def encode(self):
+        return {"name": self.name}
+
+    def msg_handler(self, msg_queue: PriorityQueue[Message]) -> bool:
+        updated = False
+        return updated
+    
+    
+class StatusEntity(Entity):
+    def __init__(self, name:str):
+        self.name = name
+        
+    def encode(self):
+        return {"name": self.name}
+
+    def msg_handler(self, msg_queue: PriorityQueue[Message]) -> bool:
+        updated = False
+        return updated
