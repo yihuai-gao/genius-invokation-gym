f4a4d58941ab24769e6f5bba15401b1d558e3e5e
David Gao <davidgao1013@gmail.com>
2023-01-08 01:34:48 +0800

    Allow agent to use elemental skill

--- gisim/cards/characters/Cryo/KamisatoAyaka.py
+++ gisim/cards/characters/Cryo/KamisatoAyaka.py
@@ -11,17 +11,73 @@ from gisim.classes.message import DealDamageMsg, Message, PaySkillCostMsg, UseSk
 if TYPE_CHECKING:
     from gisim.classes.character import CharacterEntity
 
+                
+class KamisatoArtKabuki(CharacterSkill):
+    id:int = 11051
+    name:str = "Kamisato Art: Kabuki"
+    text:str = """
+    Kamisato Art: Kabuki / 神里流·倾
+    """
+    costs: dict[ElementType, int] = {ElementType.CRYO:1, ElementType.ANY:2}
+    types: list[SkillType] = [SkillType.NORMAL_ATTACK]
+    def use_skill(self, msg_queue: PriorityQueue[Message], parent:"CharacterEntity"):
+        msg = msg_queue.get()
+        msg = cast(UseSkillMsg, msg)
+        target_player_id, target_char_pos = msg.skill_targets[0]
+        new_msg = DealDamageMsg(
+            sender_id=parent.player_id, 
+            targets=[(target_player_id, target_char_pos, ElementType.NONE, 2)],
+            )
+        msg_queue.put(new_msg)
+        
+class KamisatoArtHyouka(CharacterSkill):
+    id:int = 11052
+    name:str = "Kamisato Art: Hyouka"
+    text:str = """
+    Kamisato Art: Hyouka / 神里流·冰华
+    """
+    costs: dict[ElementType, int] = {ElementType.CRYO:3}
+    types: list[SkillType] = [SkillType.ELEMENTAL_SKILL]
+    def use_skill(self, msg_queue: PriorityQueue[Message], parent:"CharacterEntity"):
+        msg = msg_queue.get()
+        msg = cast(UseSkillMsg, msg)
+        target_player_id, target_char_pos = msg.skill_targets[0]
+        new_msg = DealDamageMsg(
+            sender_id=parent.player_id, 
+            targets=[(target_player_id, target_char_pos, ElementType.CRYO, 3)],
+            )
+        msg_queue.put(new_msg)
+        
+class KamisatoArtSoumetsu(CharacterSkill):
+    id:int = 11053
+    name:str = "Kamisato Art: Soumetsu"
+    text:str = """
+    Kamisato Art: Soumetsu / 神里流·霜灭
+    """
+    costs: dict[ElementType, int] = {ElementType.CRYO:3, ElementType.POWER:3}
+    types: list[SkillType] = [SkillType.ELEMENTAL_BURST]
+    def use_skill(self, msg_queue: PriorityQueue[Message], parent:"CharacterEntity"):
+        msg = msg_queue.get()
+        msg = cast(UseSkillMsg, msg)
+        target_player_id, target_char_pos = msg.skill_targets[0]
+        new_msg = DealDamageMsg(
+            sender_id=parent.player_id, 
+            targets=[(target_player_id, target_char_pos, ElementType.CRYO, 4)],
+            )
+        # TODO: Generate a summon
+        msg_queue.put(new_msg)
+        
 class KamisatoArtSenho(CharacterSkill):
     
     id:int = 11054
+    name:str = "Kamisato Art: Senho"
     text:str = """
     Kamisato Art: Senho / 神里流·霰步
     (Passive) When switched to be the active character, this character gains Cryo Elemental Infusion.
     【被动】此角色被切换为「出战角色」时，附属冰元素附魔。
     """
-    name:str = "Kamisato Art: Senho"
-    costs: dict[ElementType, int] = {ElementType.CRYO:1, ElementType.ANY:2}
-    types: list[SkillType] = [SkillType.NORMAL_ATTACK]
+    costs: dict[ElementType, int] = {}
+    types: list[SkillType] = [SkillType.PASSIVE_SKILL]
     
     def use_skill(self, msg_queue: PriorityQueue[Message], parent:"CharacterEntity"):
         msg = msg_queue.get()
@@ -32,17 +88,6 @@ class KamisatoArtSenho(CharacterSkill):
             targets=[(target_player_id, target_char_pos, ElementType.NONE, 2)],
             )
         msg_queue.put(new_msg)
-                
-                
-
-
-            
-            
-    # def on_skill(self) -> list:
-    # This is just a placeholder, you need to replace it with your own implementation
-    # return self._build_message(
-    #     "Inufsion", ("cryo",), conditions={"on_switch": True}
-    # )
 
 class KamisatoAyaka(CharacterCard):
     id:int = 1105
@@ -53,9 +98,5 @@ class KamisatoAyaka(CharacterCard):
     power: int = 0
     max_power: int = 3
     weapon_type: WeaponType = WeaponType.SWORD
-    skills: list[CharacterSkill] = [KamisatoArtSenho()]
+    skills: list[CharacterSkill] = [KamisatoArtKabuki(), KamisatoArtHyouka(), KamisatoArtSoumetsu(), KamisatoArtSenho()]
     
-
-# @register_character_skill_factory(11054)
-
-
